aff99bfe1ae8a128e5a1008f6173fde0cdd7da55
ksanabuka <obuksha@hotmail.com>
2019-05-07 15:50:16 -0700

    drafts

---
+++ README.md
@@ -0,0 +1,1 @@
+My implementation of C printf function
---
+++ tests.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+int main(void)
+  {
+
+      char * s = "helllo";
+   //   int number    =  5;
+
+    //printf("-%4d-and a-%10s-word.\n", number, pointer); // Here is a number-   5-and a-    little-word.
+   // printf("-%-10d-and a-%10s-word.\n", number, pointer); // -5         -and a-    little-word.
+    // printf("-%+010d-and a-%10s-word.\n", number, pointer); //  -+000000005-and a-    little-word.
+  //  printf("!%-#10x!\n", number);           //      !0x5       !
+ //   printf("!%#x!\n", number);              //      !0x5!
+  //  printf("!%*d!\n", 6, number);            //       !     5!
+   // printf("!%4.8s!\n", s); // ! hel!
+    //printf("!%4.*s!\n", 5, s);            //     !helll!
+
+
+  }
+  
