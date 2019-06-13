58d256c6583597dde8394f0ff7571d24299f80c0
ksanabuka <obuksha@hotmail.com>
2019-06-13 15:48:05 -0700

    done

--- restructure/DoublePrintTypeInfo.c
+++ restructure/DoublePrintTypeInfo.c
@@ -9,60 +9,198 @@ static char get_sign(long double value, const struct FormatParams *fmt_params) {
     return fmt_params->flags & F_PLUS ? '+' : 0;
 }
 
-static void abs_integral_quotient_to_digits(char *digits, long double value) {
+char* abs_integral_quotient_to_digits(char *digits, long double value) {
     int i;
     long long d = (long long)value;
     
     i = 0;
     while (value)
     {
-        d = value - (value / 10) * 10;
+        d = (long long)value - ((long long)value / 10) * 10;
         d = d < 0 ? -d : d;
         digits[i] = d + '0';
-        value /= 10;
+        (long long)value /= 10;
         ++i;
     }
     digits[i] = 0;
     reverse_str(digits);
+    return (digits);
 }
+
+static char * rounding(char * s)
+{
+    int len = ft_strlen(s); 
+    int i = len - 1; 
+    char * new; 
+    if (s[i] >= '5' && s[i] <= '9' && i - 1 >= 0)
+        {
+            if (s[i - 1] < '9')
+            {
+                s[i - 1] = s[i - 1] + 1;
+            }
+            else 
+            {
+                s[i - 1] = '0';
+                i = i - 2; 
+                while (i >= 0)
+                {
+                    if (s[i] < '9')
+                    {
+                        s[i] = s[i] + 1;
+                        break;
+                    }
+                    else 
+                    {
+                        s[i] = '0';
+                        if (i == 0 && s[i] == '0')
+                            return 0;
+                        i--;
+                    }
+                }
+                
+            }
+        }
+    new = ft_strnew(len - 1);
+    i = 0;
+    while (i < len - 1)
+    {
+        new[i] = s[i];
+        i++;
+    }
+    free(s);
+    return new;          
+}
+
+char * createStr0(int len)
+{
+    char * str = ft_strnew(len); 
+    int i = 0;
+    while (i < len)
+    {
+        str[i] = '0';
+        i++;
+    }
+    return str; 
+}
+
+char *fracture_part(long double * value, const struct FormatParams *fmt_params)
+{
+    char * res;
+    char * tmp; 
+    char * str;
+
+    if (fmt_params->precision == 0)
+    {    
+       str = ft_strnew(0);
+       return str;
+    }
+
+    str = ft_strnew(17);
+    int i = 0; 
+    long double aftercoma = *value - (long long)(*value);
+    while (42)
+    {
+        if (fmt_params->precision != -1 && i == fmt_params->precision + 1)
+            break;
+        if (aftercoma == 0 || (fmt_params->precision == -1 && i == 7) || i == 17)
+            break; 
+        if ((int) (aftercoma * 10) == 0 && aftercoma != 0)
+        {
+            str[i] = '0';
+        }
+        else 
+        {
+            str[i] = (int) (aftercoma * 10) + '0';
+        }
+        aftercoma = aftercoma * 10 - (int) (aftercoma * 10);
+        i++; 
+    }
+    str = rounding(str);
+    if (!str)
+    {
+        *value = *value + 1; 
+        if (fmt_params->precision == 0)
+        {
+           str = ft_strnew(0);
+                // str = createStr0(1);
+            // str[0] = '\0'; 
+        }
+        else if (fmt_params->precision == -1)
+        {
+            str = createStr0(6);
+        }
+        else if (fmt_params->precision > 0)
+        {
+            str = createStr0(fmt_params->precision);
+        }
+     }
+
+    if (fmt_params->precision > 0 && (int)ft_strlen(str) < fmt_params->precision)
+    {
+        int len1 = fmt_params->precision - (int)ft_strlen(tmp);
+        tmp = createStr0(len1);
+        res = tmp; 
+        str = ft_strjoin(str, tmp);
+        free(res);
+        free(tmp);
+    }
+
+    if (ft_strlen(str) < 6 && fmt_params->flags & F_DIEZ)
+    {
+       int len = 6 - ft_strlen(str); 
+        tmp = ft_strnew(len);
+        i = 0; 
+        while (i < len)
+        {
+            tmp[i] = '0';
+            i++;
+        }
+        res = str; 
+        str = ft_strjoin(str, tmp);
+        free(res);
+        free(tmp);
+    }
+    return str; 
+}
+
 
 static void abs_integral_and_fractural_join(char *digits, long double value, const struct FormatParams *fmt_params) {
   
     {
         char * res;
-        char * tmp = ft_strnew(0);
+        char * tmp;
+        char * fracturial_part;
+        char * integral_part;
+        
         value = (value < 0) ? -value : value;
-        char * fracturial_part =  fracture_part(&value, fmt_params);
-        char * integral_part = ft_itoa_base((long long) d, 10);
-        
-        
-        
-        if (fracturial_part[0] != '\0' && params->precision != 0)
+        fracturial_part =  fracture_part(&value, fmt_params);
+        integral_part = abs_integral_quotient_to_digits(digits, value);
+        if (fracturial_part[0] != '\0' && fmt_params->precision != 0)
         {
+            tmp = ft_strnew(1);
             tmp[0] = '.';
             res = integral_part;
             integral_part = ft_strjoin(integral_part, tmp);
             free(res);
             free(tmp);
         }
-        
-        res = ft_strjoin(integral_part, fracturial_part);
+        tmp = digits;
+        free(tmp); 
+        digits = ft_strjoin(integral_part, fracturial_part);
         free(integral_part);
         free(fracturial_part);
-
 }
 
 
-
-struct PrintTypeInfo create_int_print_type_info(void *value_ptr, const struct FormatParams *fmt_params) {
+struct PrintTypeInfo create_real_print_type_info(void *value_ptr, const struct FormatParams *fmt_params) {
     struct PrintTypeInfo res;
-    long long value;
+    long double value;
     
-    value = *(long long *)value_ptr;
+    value = *(long double *)value_ptr;
     res.sign = get_sign(value, fmt_params);
-    res.value_str = ft_strnew(LONG_LONG_BITS);
+    res.value_str = ft_strnew(0);
     res.free_value_str = 1;
-    abs_ll_to_digits(res.value_str, value, fmt_params->precision);
+    abs_integral_and_fractural_join(res.value_str, value, fmt_params);
     ft_strcpy(res.prefix, "");
     res.num_leading_zeros = 0;
     if (fmt_params->precision > (int)ft_strlen(res.value_str)) {
